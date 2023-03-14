c032141ecc2befdbdb30e6bc65bd656a88c3e8d8
oaoong <oaoong@github.com>
2023-03-14 15:48:16 +0900

    scss /close #7

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   distDir: ".next",
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src/styles")],
+    prependData: `@import "src/styles/_variables.scss"; @import "src/styles/_mixins.scss";`,
+  },
   webpack: (config, { isServer }) => {
     const prod = process.env.NODE_ENV === "production";
     return {
