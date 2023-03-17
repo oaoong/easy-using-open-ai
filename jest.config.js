60856ede73feb48df2ca359e23b3c49196d41305
oaoong <oaoong@github.com>
2023-03-17 15:38:19 +0900

    jest config

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,32 @@
-const nextJest = require("next/jest");
+const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: "./",
+    // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+    dir: './',
 });
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/$1",
-    "^@pages/(.*)$": "<rootDir>/pages/$1",
-    "^@public/(.*)$": "<rootDir>/public/$1",
-    "^@components/(.*)$": "<rootDir>/src/components/$1",
-    "^@styles/(.*)$": "<rootDir>/src/styles/$1",
-    "^@utils/(.*)$": "<rootDir>/src/utils/$1",
-    "^@hooks/(.*)$": "<rootDir>/src/hooks/$1",
-    "^@types/(.*)$": "<rootDir>/src/types/$1",
-  },
-  testEnvironment: "jest-environment-jsdom",
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/$1',
+        '^@pages/(.*)$': '<rootDir>/pages/$1',
+        '^@public/(.*)$': '<rootDir>/public/$1',
+        '^@assets/(.*)$': '<rootDir>/assets/$1',
+        '^@components/(.*)$': '<rootDir>/src/components/$1',
+        '^@styles/(.*)$': '<rootDir>/src/styles/$1',
+        '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+        '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+        '^@types/(.*)$': '<rootDir>/src/types/$1',
+    },
+    testEnvironment: 'jest-environment-jsdom',
+    testMatch: [
+        '**/__tests__/**/*.js?(x)',
+        '**/?(*.)+(spec|test).js?(x)',
+        '**/?(*.)+(spec|test).ts?(x)',
+        '**/?(*.)+(spec|test).tsx?(x)',
+    ],
+    testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
