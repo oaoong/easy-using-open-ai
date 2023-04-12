fbc937b9c6c3c729e91d4eae2b7b080dd7059a56
oaoong <oaoong@github.com>
2023-04-12 15:44:12 +0900

    change menu ui

--- src/components/template/home/homeTemplate.tsx
+++ src/components/template/home/homeTemplate.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import { useRecoilState } from 'recoil';
 
 import LinkButtonList, {
@@ -7,11 +7,14 @@ import LinkButtonList, {
 } from '@/src/components/organisms/linkButtonList';
 import { pageState } from '@/src/recoil/atom';
 import Ask from '../../organisms/ask';
+import Blank from '../../organisms/blank';
 import Keyword from '../../organisms/keyword';
 import Likes from '../../organisms/likes';
 import Refactoring from '../../organisms/refactoring';
 import Summary from '../../organisms/summary';
 import TitleMaker from '../../organisms/titleMaker';
+
+import './homeTemplate.scss';
 
 export default function HomeTemplate({
     buttonData,
@@ -19,7 +22,7 @@ export default function HomeTemplate({
     const [page, setPage] = useRecoilState(pageState);
 
     const pageList = [
-        <LinkButtonList buttonData={buttonData} setPage={setPage} key={0} />,
+        <Blank key={0} />,
         <Summary key={1} />,
         <Refactoring key={2} />,
         <Likes key={3} />,
@@ -28,5 +31,26 @@ export default function HomeTemplate({
         <TitleMaker key={6} />,
     ];
 
-    return pageList[page];
+    return (
+        <div className='home-template-container'>
+            <CSSTransition
+                in={page !== 0}
+                appear={true}
+                timeout={500}
+                classNames='slide'
+                key={buttonData.length}
+            >
+                <div className='button-list'>
+                    <LinkButtonList
+                        buttonData={buttonData}
+                        setPage={setPage}
+                        key={0}
+                    />
+                </div>
+            </CSSTransition>
+            <div className={`page ${page !== 0 && 'show'}`}>
+                {pageList[page]}
+            </div>
+        </div>
+    );
 }
