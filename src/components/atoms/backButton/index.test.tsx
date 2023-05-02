44e486c54375a0569c6a8924e2c0f95df18e1f4b
oaoong <oaoong@github.com>
2023-05-02 22:33:42 +0900

    change test code

--- src/components/atoms/backButton/index.test.tsx
+++ src/components/atoms/backButton/index.test.tsx
@@ -1,19 +1,30 @@
 import { render, screen, fireEvent } from '@testing-library/react';
+import { RecoilRoot } from 'recoil';
 import BackButton from '.';
 
 describe('BackButton 컴포넌트', () => {
     test('renders back button', () => {
-        render(<BackButton />);
+        render(
+            <RecoilRoot>
+                <BackButton />
+            </RecoilRoot>,
+        );
         const backButton = screen.getByRole('button', { name: 'back-button' });
         expect(backButton).toBeInTheDocument();
     });
 
     test('clicking back button calls setPageNumber', () => {
         const setPageNumber = jest.fn();
-        jest.spyOn(require('recoil'), 'useSetRecoilState').mockReturnValue(
+        const pageNumber = 1;
+        jest.spyOn(require('recoil'), 'useRecoilState').mockReturnValue([
+            pageNumber,
             setPageNumber,
+        ]);
+        render(
+            <RecoilRoot>
+                <BackButton />
+            </RecoilRoot>,
         );
-        render(<BackButton />);
         const backButton = screen.getByRole('button', { name: 'back-button' });
         fireEvent.click(backButton);
         expect(setPageNumber).toHaveBeenCalledWith(0);
